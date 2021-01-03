5e1065c4eb910aa2ed63f78531b3b18c805d4401
Güven Can Yıldız <>
2021-01-04 01:43:01 +0300

    error screen

---
+++ BilgiYarismasi/Assets/Ekmekk/Scripts/ErrorScene/ErrorUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ErrorUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI txt_error,txt_errorMessage,txt_header;
+    [SerializeField] private Button button;
+
+    private void Awake()
+    {
+        txt_header.text = "Eyvah!";
+        txt_error.text = "Bir şeyler ters gitti ve sorun tarafımıza iletildi. En kısa zamanda çözeceğiz";
+
+        txt_errorMessage.text = PlayerPrefs.GetString("errorMessage", "");
+        
+        button.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene((int) Scenes.Opening);
+        });
+    }
+}
