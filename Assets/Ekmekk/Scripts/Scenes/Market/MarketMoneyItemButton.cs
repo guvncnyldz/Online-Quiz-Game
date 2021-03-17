8424c991adf3c63e20e2e06d3e2bf07c0e370ec5
Güven Can Yıldız <yildiz.guvencan@gmail.com>
2021-03-17 21:59:54 +0300

    IAP

--- Assets/Ekmekk/Scripts/Scenes/Market/MarketMoneyItemButton.cs
+++ Assets/Ekmekk/Scripts/Scenes/Market/MarketMoneyItemButton.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class MarketMoneyItemButton : MonoBehaviour
 {
     public int count;
-
+    public string id;
     public void Use(Action onUse)
     {
-        User.GetInstance().Money += count;
-        onUse?.Invoke();
+        FindObjectOfType<IAP>().BuyConsumable(id, () =>
+        {
+            User.GetInstance().Money += count;
+            onUse?.Invoke();
+        });
     }
 }
